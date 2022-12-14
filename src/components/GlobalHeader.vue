86ab768f36360d5ade46bd78c5c0c318396b6a2c
LiaoKong <568250549@qq.com>
2022-12-14 23:06:30 +0800

    完成 DropDown 相关组件

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -6,13 +6,21 @@
       <li class="list-inline-item"><a href="#" class="btn btn-outline-light my-2">注册</a></li>
     </ul>
     <ul v-else class="list-inline mb-0">
-      <li class="list-inline-item"><a href="#" class="btn btn-outline-light my-2">你好，{{user.name}}</a></li>
+      <li class="list-inline-item">
+        <drop-down :title="`你好，${user.name}`">
+          <dropdown-item><a href="#" class="dropdown-item">新建文章</a></dropdown-item>
+          <dropdown-item disabled><a href="#" class="dropdown-item">编辑资料</a></dropdown-item>
+          <dropdown-item><a href="#" class="dropdown-item">退出登录</a></dropdown-item>
+        </drop-down>
+      </li>
     </ul>
   </nav>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
+import DropDown from '@/components/DropDown.vue'
+import DropdownItem from '@/components/DropdownItem.vue'
 
 export interface UserProps {
   isLogin: boolean;
@@ -22,6 +30,10 @@ export interface UserProps {
 
 export default defineComponent({
   name: 'GlobalHeader',
+  components: {
+    DropDown,
+    DropdownItem
+  },
   props: {
     user: {
       type: Object as PropType<UserProps>,
