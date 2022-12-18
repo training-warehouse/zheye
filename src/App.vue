f51bf08b21050ba74034f126050a60058c13791b
LiaoKong <568250549@qq.com>
2022-12-18 23:20:28 +0800

    添加 PostList 页面

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,41 @@
 <template>
   <div class="container">
     <global-header :user="user"></global-header>
-    <validate-form action="" @form-submit="onFormSubmit">
-      <div class="mb-3">
-        <label class="from-label">邮箱地址</label>
-        <validate-input :rules="emailRules" v-model="emailVal" type="text" placeholder="请输入邮箱地址"></validate-input>
-      </div>
-      <div class="mb-3">
-        <label class="form-label">密码</label>
-        <validate-input :rules="passwordRules" v-model="passwordVal" type="password"
-                        placeholder="请输入密码"></validate-input>
-      </div>
-      <template #submit>
-        <button class="btn btn-danger">提交</button>
-      </template>
-    </validate-form>
+    <router-view></router-view>
+    <footer class="text-center py-4 text-secondary bg-light mt-6">
+      <small>
+        <ul class="list-inline mb-0">
+          <li class="list-inline-item">2022 者也专栏</li>
+          <li class="list-inline-item">课程</li>
+          <li class="list-inline-item">文档</li>
+          <li class="list-inline-item">联系</li>
+          <li class="list-inline-item">更多</li>
+        </ul>
+      </small>
+    </footer>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
-// import ColumnList, { ColumnProps } from '@/components/ColumnList.vue'
+
 import GlobalHeader, { UserProps } from '@/components/GlobalHeader.vue'
-import ValidateInput, { RulesProp } from '@/components/ValidateInput.vue'
-import ValidateForm from '@/components/ValidateForm.vue'
 
 const user: UserProps = {
-  isLogin: true,
-  name: 'liaokong'
+  isLogin: false,
+  name: ''
 }
 
 export default defineComponent({
   name: 'App',
   components: {
-    // ColumnList,
-    GlobalHeader,
-    ValidateInput,
-    ValidateForm
+    GlobalHeader
   },
   setup () {
-    const emailVal = ref('')
-    const passwordVal = ref('')
-
-    const emailRules: RulesProp = [
-      {
-        type: 'required',
-        message: '邮箱地址不能为空'
-      },
-      {
-        type: 'email',
-        message: '邮箱地址格式不正确'
-      }
-    ]
-    const passwordRules: RulesProp = [
-      {
-        type: 'required',
-        message: '密码不能为空'
-      }
-    ]
-
-    const onFormSubmit = (result: boolean) => {
-      console.log(result)
-    }
-
     return {
       // list: testData,
-      user,
-      emailRules,
-      emailVal,
-      passwordVal,
-      passwordRules,
-      onFormSubmit
+      user
     }
   }
 })
---
+++ src/components/PostList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="post-list">
+    <article v-for="post in list" :key="post._id" class="card mb-3 shadow-sm">
+      <div class="card-body">
+        <h4>
+          <router-link :to="`/posts/${post._id}/`">{{ post.title }}</router-link>
+        </h4>
+        <div class="row my-3 align-items-center">
+          <div v-if="post.image && typeof post.image !== 'string'" class="col-4">
+            <img :src="post.image" :alt="post.title" class="rounded-lg w-100">
+          </div>
+          <p :class="{'col-8': post.image}" class="text-muted">{{ post.content }}</p>
+        </div>
+        <span class="text-muted">{{ post.createdAt }}</span>
+      </div>
+    </article>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    list: {
+      required: true,
+      type: Array
+    }
+  }
+})
+</script>
+
+<style scoped>
+.post-list h4 a {
+  text-decoration: none;
+  color: #1a1a1a;
+}
+
+.post-list h4 a:hover {
+  color: #0d6efd;
+}
+</style>
